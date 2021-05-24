799488df43adbd615754658a3881a81386a587e7
jamieando <jamie_anderson81@hotmail.com>
2021-05-24 11:44:14 +1000

    Added navbar and footer components to homepage

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, ex. Neque
+        pariatur, aperiam harum quo laudantium dicta reiciendis iste officia vel
+        libero eos nam aspernatur maxime dignissimos incidunt, saepe fuga.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, ex. Neque
+        pariatur, aperiam harum quo laudantium dicta reiciendis iste officia vel
+        libero eos nam aspernatur maxime dignissimos incidunt, saepe fuga.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+        quasi incidunt distinctio provident iure corrupti molestiae quidem
+        similique, temporibus, deleniti expedita ex enim? Pariatur ex eum
+        laborum obcaecati eaque soluta.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+        quasi incidunt distinctio provident iure corrupti molestiae quidem
+        similique, temporibus, deleniti expedita ex enim? Pariatur ex eum
+        laborum obcaecati eaque soluta.
+      </p>
+      <Footer />
     </div>
   );
 }
