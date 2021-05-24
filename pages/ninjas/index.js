6e6bab66fc2e08172eb31863e2873bcf7a0f41aa
jamieando <jamie_anderson81@hotmail.com>
2021-05-24 15:16:59 +1000

    Added dynamic route to display user specific page

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../../styles/Ninjas.module.css';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -20,11 +21,11 @@ const Ninjas = ({ ninjas }) => {
       <div>
         <h1>All Ninjas</h1>
         {ninjas.map((ninja) => (
-          <div key={ninja.id}>
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
             <a className={styles.single}>
               <h3>{ninja.name}</h3>
             </a>
-          </div>
+          </Link>
         ))}
       </div>
     </>
