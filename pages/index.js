5a004e56b0d24e470b6e47b7f2533a579dfe2720
jamieando <jamie_anderson81@hotmail.com>
2021-05-24 13:20:01 +1000

    Created and added layout file

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
@@ -20,7 +20,9 @@ export default function Home() {
         similique, temporibus, deleniti expedita ex enim? Pariatur ex eum
         laborum obcaecati eaque soluta.
       </p>
-      <Footer />
+      <Link href='/ninjas'>
+        <a>See Ninja Listing</a>
+      </Link>
     </div>
   );
 }
