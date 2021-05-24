c458ba61790c8fbf1adf2808a6b0f01b87aa3b15
jamieando <jamie_anderson81@hotmail.com>
2021-05-24 14:47:52 +1000

    Added image logo and metadata to the pages

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,29 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
-        quasi incidunt distinctio provident iure corrupti molestiae quidem
-        similique, temporibus, deleniti expedita ex enim? Pariatur ex eum
-        laborum obcaecati eaque soluta.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
-        quasi incidunt distinctio provident iure corrupti molestiae quidem
-        similique, temporibus, deleniti expedita ex enim? Pariatur ex eum
-        laborum obcaecati eaque soluta.
-      </p>
-      <Link href='/ninjas'>
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          quasi incidunt distinctio provident iure corrupti molestiae quidem
+          similique, temporibus, deleniti expedita ex enim? Pariatur ex eum
+          laborum obcaecati eaque soluta.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          quasi incidunt distinctio provident iure corrupti molestiae quidem
+          similique, temporibus, deleniti expedita ex enim? Pariatur ex eum
+          laborum obcaecati eaque soluta.
+        </p>
+        <Link href='/ninjas'>
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
