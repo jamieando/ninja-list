ef2dbda7fb404660370ebb21bdc5df2260b7c9bd
jamieando <jamie_anderson81@hotmail.com>
2021-05-24 15:07:05 +1000

    Added getStaticProps to fetch data from the API, then display the users on listings

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,8 +1,33 @@
-const Ninjas = () => {
+import Head from 'next/head';
+import styles from '../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
-    <div>
-      <h1>All Ninjas</h1>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Ninja Listing</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
